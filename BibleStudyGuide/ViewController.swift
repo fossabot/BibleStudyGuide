f945b26c65d4063997105763a134180358d27e84
Dev Agani <dev.things.ke@gmail.com>
2021-04-08 10:04:14 +0300

    chore(init): initial project setup

---
+++ BibleStudyGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BibleStudyGuide
+//
+//  Created by George Nyakundi on 08/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
